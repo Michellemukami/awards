d16feda9cc1b5b401ff64368ad8d3bd9c8c4048f
Michelle Mukami <kamimello463@gmail.com>
2019-09-05 15:42:15 +0300

    added serializer.py

--- kamiward/urls.py
+++ kamiward/urls.py
@@ -4,14 +4,17 @@ from django.conf.urls import include,url
 from . import views
 
 urlpatterns=[
-    url(r'api/merch/merch-id/(?P<pk>[0-9]+)/$',
-        views.MerchDescription.as_view()),
-    url(r'^$',views.news_today,name='newsToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+    url(r'api/prof;e/profle-id/(?P<pk>[0-9]+)/$',
+        views.ProfleDescription.as_view()),
+    url(r'api/project/project-id/(?P<pk>[0-9]+)/$',
+        views.ProjectDescription.as_view()),
+    url(r'^$',views.home,name='home'),
+    url(r'^profile/',views.new_profile,name ='newprofile'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^article/(\d+)',views.article,name ='article'),
+   
     url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^api/merch/', views.MerchList.as_view()),
+    url(r'^api/profile/', views.ProfileList.as_view()),
+    url(r'^api/project/', views.ProjectList.as_view()),
     url(r'^article/',views.new_article,name ='newsarticle'),
     url(r'^ajax/newsletter/$', views.newsletter, name='newsletter')
 ]
--- kamiward/views.py
+++ kamiward/views.py
@@ -92,3 +92,78 @@ def comment(request):
     else:
         form = CommentForm()
     return render(request, 'comment.html', {"form": form}) 
+
+
+class ProjectList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):
+        all_project = Project.objects.all()
+        serializers = AwardSerializer(all_project, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = AwardSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProjectDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_project(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = AwardSerializer(project)
+        return Response(serializers.data)
+    def put(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = AwardSerializer(project, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, format=None):
+        project = self.get_project(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):
+        all_user = Profile.objects.all()
+        serializers = UserSerializer(all_user, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = UserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_user(self, pk):
+        try:
+            return Profile.objects.get(pk=pk)
+        except Profile.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_user(pk)
+        serializers = UserSerializer(merch)
+        return Response(serializers.data)
+    def put(self, request, pk, format=None):
+        user = self.get_user(pk)
+        serializers = UserSerializer(merch, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, format=None):
+        user = self.get_user(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
